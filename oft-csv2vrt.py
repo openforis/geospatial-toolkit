2cbee991aa89a63ca6cf176a3b93d53a4f943d30
ingdanielguerrero <dfgm2006@gmail.com>
2020-03-03 18:03:43 +0100

    Update oft-csv2vrt.py (#1)
    Translated and tested in Python3

--- oft-csv2vrt.py
+++ oft-csv2vrt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Conversion of csv / space separated text file
 # into a vrt data set
 # Author: anssi.pekkarinen@fao.org
@@ -6,119 +6,64 @@
 
 import sys
 import os
-try:
-    from osgeo import gdal
-except ImportError:
-    import gdal
+import argparse
 
-gdal.AllRegister()
+parser = argparse.ArgumentParser()
+parser.add_argument("layer",
+                    help="Name of the point layer")
+parser.add_argument("x", type=int,
+                    help="Number of the x coordinate column")
+parser.add_argument("y", type=int,
+                    help="Number of the y coordinate column")
+parser.add_argument("varcol", type=int,
+                    help="Number of the z coordinate column")
+parser.add_argument("separator",
+                    help="Specify the separator used in the csv file")
+parser.add_argument("header", type=int, choices=[0, 1],
+                    help="Specify whether the file has or not a header")
+parser.add_argument("infile",
+                    help="Specify the input file path")
+parser.add_argument("outfile",
+                    help="Specify the ouput file path ending with .vrt")
+args = parser.parse_args()
 
-argv=None
-
-nr = 0
-header=0
-   
-if argv is None:
-    argv = sys.argv
-    argv = gdal.GeneralCmdLineProcessor( argv )
-    if argv is None:
-        sys.exit( 0 )
-        
-if argv < 14 or argv > 15:
-    print "Usage: oft-csv2vrt <-l layer> <-x xcol> <-y ycol> <-v varcol> <-s separator> [-h header] <-i infile> <-o outfile>"
-    sys.exit( 0 )
-
-# process arguments 
-
-i=1
-
-xcol=0;
-ycol=0;
-vcol=0;
-hdr=0
-   
-
-while i < len(argv):
-
-    print i,argv[i]
-    
-    arg = argv[i]
-    
-    if arg == '-o':
-        i = i + 1
-        outfile = argv[i]
-        
-    elif arg == '-i':
-        i = i + 1
-        infile = argv[i]
-        
-    elif arg == '-x':
-        i = i + 1
-
-        xcol=int(argv[i]);
-        
-
-    elif arg == '-y':
-        i = i+ 1
-        ycol=int(argv[i]);
-
-
-    elif arg == '-l':
-        i = i+ 1
-        layer=argv[i];
-
-    elif arg == '-v':
-        i = i+ 1
-        vcol=int(argv[i]);
-        
-    elif arg == '-s':
-        i = i+ 1
-        sep=argv[i];
-        
-    elif arg == '-h':
-        hdr=1
-        
-    elif arg[:1] == '-':
-        print('Unrecognised option: %s' % arg)
-        Usage()
-        sys.exit( 1 )
-
-    i=i+1
-
+xcol = args.x
+ycol = args.y
+vcol = args.varcol
+hdr = args.header
+sep = args.separator
+infile = args.infile
+outfile = args.outfile
+layer = args.layer
 
 try:
-
-    print "Opening files"
+    print("Opening files")
     input=open(infile,'r');
     output=open(outfile,'w');
 
 except IOError:
-    print 'Can\'t open file.'
+    print('Can\'t open file.')
     sys.exit(0)
-    
 
-print >> output, '<OGRVRTDataSource>'
-print >> output, '<OGRVRTLayer name="'+layer+'">'
-print >> output, '<SrcDataSource>'+infile+'</SrcDataSource>'
-print >> output, '<GeometryType>wkbPoint</GeometryType>'
+
+print('<OGRVRTDataSource>', file=output)
+print('<OGRVRTLayer name="'+layer+'">', file=output)
+print('<SrcDataSource>'+infile+'</SrcDataSource>', file=output)
+print('<GeometryType>wkbPoint</GeometryType>', file=output)
 
 
 line=names=input.readline().rstrip()
 cols=line.split(sep)
 
-
-
 if hdr == 1:
-    print "Using header"
-    print >> output, '<GeometryField encoding="PointFromColumns" x="'+str(cols[xcol-1])+'" y="'+str(cols[ycol-1])+'" z="'+str(cols[vcol-1])+'"/>'
+    print("Using header")
+    print('<GeometryField encoding="PointFromColumns" x="'+str(cols[xcol-1])+'" y="'+str(cols[ycol-1])+'" z="'+str(cols[vcol-1])+'"/>', file=output)
 else:
-    print >> output, '<GeometryField encoding="PointFromColumns" x="field_'+str(xcol)+'" y="field_'+str(ycol)+'" z="field_'+str(vcol)+'"/>'
-    
+    print('<GeometryField encoding="PointFromColumns" x="field_'+str(xcol)+'" y="field_'+str(ycol)+'" z="field_'+str(vcol)+'"/>', file=output)
 
-print >> output, '</OGRVRTLayer>'
-print >> output, '</OGRVRTDataSource>'
-
+print('</OGRVRTLayer>', file=output)
+print('</OGRVRTDataSource>', file=output)
+print('File vrt created.')
 
 input.close()
 output.close()
-

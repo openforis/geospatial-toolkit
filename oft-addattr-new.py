8791f5587df9ed9c91a54374895c691a419af145
ingdanielguerrero <dfgm2006@gmail.com>
2020-03-03 16:50:13 +0100

    Update oft-addattr-new.py
    Code translated to Python3. The arguments to the script has been changed with the module parser and also been created some useful help tips for each one.
    The names of some variables with reserved names have been renamed with other names.

--- oft-addattr-new.py
+++ oft-addattr-new.py
@@ -8,7 +8,7 @@
 # AP 11 Oct 2012
 # Changed usage to allow user to select JoinAttrName
 # Fixed a bug in assigning new values for fields
-# And using dictionary instead of a list to allow 
+# And using dictionary instead of a list to allow
 # different number of feature infile records
 # RH 30 Jan 2013
 # Changed process stage controlling so that it does not
@@ -17,28 +17,34 @@
 
 from osgeo import ogr
 import sys
+import argparse
 
 # Open a Shapefile, and get field names
 
-if len(sys.argv) < 6:
-    print 'Usage: oft-addattr.py <shapefile> <JoinAttrName> <NewAttrName> <NewAttrType> <textfile> [NoDataVAlue]'
-    print 'Allowed field types are:'
-    print 'Int/Str/Float'
-    sys.exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument("shapefile",
+                    help="Path of the shapefile")
+parser.add_argument("joinAttrName",
+                    help="Name of the column to be used as a join")
+parser.add_argument("newAttrName",
+                    help="Name of the new column to be created")
+parser.add_argument("NewAttrType", choices = ['Int', 'Float', 'Str'],
+                    help="Type of the attribute to be created and joined")
+parser.add_argument("textFile",
+                    help="Path of the file to be joined, delimited by spaces")
+parser.add_argument("-nd", "--NoDataVAlue", default = -9999,
+                    help="Value to be used when there is no data, \
+                    if not specified the defauld value is -9999")
+args = parser.parse_args()
 
-source = ogr.Open(sys.argv[1], 1)
-JoinAttrName=sys.argv[2]
-NewAttrName=sys.argv[3]
-NewAttrType=sys.argv[4]
-input=open(sys.argv[5],'r');
+source = ogr.Open(args.shapefile, 1)
+JoinAttrName = args.joinAttrName
+NewAttrName = args.newAttrName
+NewAttrType = args.NewAttrType
+input_file = open(args.textFile,'r')
+no_data = args.NoDataVAlue
 
-if len(sys.argv) == 7:
-    NoData=int(sys.argv[6]);
-else:
-    NoData=-9999
-
-
-layer = source.GetLayer()      
+layer = source.GetLayer()
 layer_defn = layer.GetLayerDefn()
 field_names = [layer_defn.GetFieldDefn(i).GetName() for i in range(layer_defn.GetFieldCount())]
 
@@ -46,20 +52,20 @@ field_names = [layer_defn.GetFieldDefn(i).GetName() for i in range(layer_defn.Ge
 
 #RH: numRows was giving one too large a nbr
 #numRows=len(input.readlines())+1;
-numRows=len(input.readlines());
-input.seek(0)
-lst={}
+numRows = len(input_file.readlines())
+input_file.seek(0)
+lst = {}
 
 numFeatures = layer.GetFeatureCount()
 
-print "Reclass Rows ",numRows;
-print "Shape Features ",numFeatures
+print("Reclass Rows ", numRows)
+print("Shape Features ", numFeatures)
 
-for word in input.xreadlines():
-    cols=word.split(' ')    
-    id=int(cols[0])    
-    outval=cols[1]
-    lst[id]=outval
+for word in input_file:
+    cols = word.split(' ')
+    key = int(cols[0])
+    outval = cols[1]
+    lst[key] = outval
 
 
 # Add a new field as an integer
@@ -71,8 +77,8 @@ elif NewAttrType == 'Float':
 elif NewAttrType == 'Str':
     new_field = ogr.FieldDefn(NewAttrName, ogr.OFTString)
 else:
-    print 'Unknown field type'
-    exit
+    print('Unknown field type')
+    exit()
 
 layer.CreateField(new_field)
 
@@ -80,32 +86,32 @@ feature = layer.GetNextFeature()
 
 count=0
 progr=0;
-print "Processing. Please wait"
+print("Processing. Please wait")
 
 while feature:
 
     segid = int(feature.GetField(JoinAttrName))
-        
+
     if segid in lst:
-        feature.SetField(NewAttrName,lst[segid])   
+        feature.SetField(NewAttrName,lst[segid])
     else:
-        feature.SetField(NewAttrName,NoData)   
+        feature.SetField(NewAttrName,no_data)
 
     layer.SetFeature(feature)
 
-    count = count + 1 
+    count = count + 1
 #RH: this crashed with < 10 polygons, changed so that we do not go there
 # in such cases
     if(numFeatures >= 100):
         if(count % (numFeatures/10) == 0):
-            progr=progr+1        
-            sys.stdout.write(str(progr));
-            sys.stdout.write(" ");
+            progr=progr+1
+            sys.stdout.write(str(progr))
+            sys.stdout.write(" ")
             sys.stdout.flush()
 
     feature = layer.GetNextFeature()
 
-sys.stdout.write("Done\n");
+sys.stdout.write("Done\n")
 
 # Close the Shapefile
 source.Destroy()
